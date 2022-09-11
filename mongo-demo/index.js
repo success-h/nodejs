61b5d4472fed005ea96895fb9a1525fb33102f4d
Success Hycenth <successhycenth8@gmail.com>
2022-09-11 20:25:05 +0100

    nodejs

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 mongoose
   .connect("mongodb://localhost/playground")
   .then(() => console.log("Connected to MongoDb"))
-  .catch((err) => console.error("could not  connect to Mongodb..."));
+  .catch((err) => console.error("could not  connect to Mongodb...", { err }));
 
 const courseSchema = new mongoose.Schema({
   name: String,
@@ -17,10 +17,9 @@ const Course = mongoose.model("Course", courseSchema);
 
 async function createCouse() {
   const course = new Course({
-    name: "Angular course",
+    name: "Node course",
     author: "Mosh",
-
-    tags: ["Angular", "Frontend"],
+    tags: ["node", "Backend"],
     isPoblished: true,
   });
 
@@ -28,4 +27,12 @@ async function createCouse() {
   console.log(result);
 }
 
-createCouse();
+async function getCourses() {
+  const courses = await Course.find({ author: "Mosh", isPoblished: true })
+    .limit(10)
+    .sort({ name: 1 })
+    .select({ name: 1, tags: 1 });
+  console.log("course:", courses);
+}
+
+getCourses();
